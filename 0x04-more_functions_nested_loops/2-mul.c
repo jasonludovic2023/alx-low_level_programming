27a2d99ea85dee66e4cd4e4d434bcee6384d360b
jasonludovic2023 <kwaaureal6@gmail.com>
2022-12-16 14:03:42 +0000

    multiplication oftwo intenger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - is multication of two intenger
+ * @a: the first  intenger
+ * @b: the second intenger
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,6 +15,7 @@ void print_to_98(int n);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 
 #endif
