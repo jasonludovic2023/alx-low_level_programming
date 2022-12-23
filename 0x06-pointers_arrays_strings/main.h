a2eb67f9bde5c0168a6f7692b037990a19193b4b
jasonludovic2023 <kwaaureal6@gmail.com>
2022-12-23 12:03:32 +0000

    Task2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -24,5 +24,6 @@ void _puts(char *str);
 void print_rev(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
