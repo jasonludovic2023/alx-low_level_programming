6fd76835b3fc41a9fc1bf5896d6ad6bba1945b33
jasonludovic2023 <kwaaureal6@gmail.com>
2023-03-16 12:25:48 -0700

    puts and printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this programm print Programming with function put
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
