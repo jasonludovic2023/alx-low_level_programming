3d9f40155ee6be302528523b6b7252c3bb18c69d
jasonludovic2023 <kwaaureal6@gmail.com>
2023-03-16 13:50:12 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this program show the size of variable
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
