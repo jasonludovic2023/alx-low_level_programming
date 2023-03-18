b3c8e44e140fdf7a7c93be27d8dfaee889bd0d8d
jasonludovic2023 <kwaaureal6@gmail.com>
2023-03-17 18:20:29 -0700

    print alphabet lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char ALP[26] = "ABCDEFGHIJKLMOPQRSTUVWYXYZ";
+	int i;
+	int j;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+
+	for (j = 0; j < 26; j++)
+	{
+		putchar(ALP[j]);
+	}
+	putchar('\n');
+	return (0);
+}
